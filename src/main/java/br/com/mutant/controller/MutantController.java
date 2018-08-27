56d8541858f05ea597b980e0bc030e1464af063f
vanessanv <nessa.vnv@gmail.com>
2018-08-26 23:11:13 -0300

    configuracao do controller

--- src/main/java/br/com/mutant/controller/MutantController.java
+++ src/main/java/br/com/mutant/controller/MutantController.java
@@ -6,14 +6,14 @@ import java.util.LinkedHashMap;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 import br.com.mutant.validation.MutantValidation;
 
-@RestController
+@Controller
 public class MutantController {
 
 	@PostMapping(path = "/mutant", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
