25aea15fd81254e681b2a895f7b4153110796b0a
vanessanv <nessa.vnv@gmail.com>
2018-08-26 23:04:28 -0300

    alteracao de configuracoes

--- src/main/java/br/com/mutant/controller/MutantApplication.java
+++ src/main/java/br/com/mutant/controller/MutantApplication.java
@@ -1,18 +1,7 @@
 package br.com.mutant.controller;
 
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import br.com.mutant.validation.MutantValidation;
 
 @SpringBootApplication
 public class MutantApplication {
--- src/main/java/br/com/mutant/controller/MutantController.java
+++ src/main/java/br/com/mutant/controller/MutantController.java
@@ -2,21 +2,18 @@ package br.com.mutant.controller;
 
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import br.com.mutant.validation.MutantValidation;
 
-@Controller
+@RestController
 public class MutantController {
 
 	@PostMapping(path = "/mutant", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
